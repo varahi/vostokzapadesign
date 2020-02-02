140ceb8b9aa647ecba70516fa614d3bed872e305
Dmitry Vasilev <dmitry@t3dev.ru>
2020-02-02 13:32:07 +0300

    Feature #368 - EXT:tmpl - layouts, templates and partials

--- public_html/typo3conf/ext/tmpl/ext_tables.sql
+++ public_html/typo3conf/ext/tmpl/ext_tables.sql
@@ -2,6 +2,6 @@
 # Table structure for table 'tt_content'
 #
 CREATE TABLE tt_content (
-  #spaceBefore smallint (5) DEFAULT '' NOT NULL,
-  #spaceAfter smallint (5) DEFAULT '' NOT NULL,
+  spaceBefore smallint (5) DEFAULT '' NOT NULL,
+  spaceAfter smallint (5) DEFAULT '' NOT NULL,
 );
