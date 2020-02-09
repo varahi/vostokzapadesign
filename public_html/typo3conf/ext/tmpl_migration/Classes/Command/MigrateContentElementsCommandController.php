bce81cbd05e62ba88e3d0ac5b88baae7599af97a
Dmitry Vasilev <dmitry@t3dev.ru>
2020-02-09 14:43:33 +0300

    Feature #369 - add new empty class MigrateContentElementsCommandController

---
+++ public_html/typo3conf/ext/tmpl_migration/Classes/Command/MigrateContentElementsCommandController.php
@@ -0,0 +1,22 @@
+<?php
+namespace  T3Dev\TmplMigration\Command;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * MigrateContentElementsCommandController
+ */
+
+class MigrateContentElementsCommandController extends AbstractCommandController
+{
+
+    /**
+     * Migrate content elements command
+     *
+     * @return void
+     */
+    public function migrateContentElementsCommand() {
+
+    }
+
+}
