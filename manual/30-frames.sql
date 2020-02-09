83a2e83eac90d5690123835953cd55a2b443c847
Dmitry Vasilev <dmitry@t3dev.ru>
2020-02-09 17:05:01 +0300

    Updating the manual

---
+++ manual/30-frames.sql
@@ -0,0 +1,11 @@
+UPDATE tt_content
+SET frame_class = '25'
+WHERE frame_class = 'custom-25';
+
+UPDATE tt_content
+SET frame_class = '26'
+WHERE frame_class = 'custom-26';
+
+UPDATE tt_content
+SET frame_class = '27'
+WHERE frame_class = 'custom-27';
