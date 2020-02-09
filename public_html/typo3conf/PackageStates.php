ba9ef87aa550e4abb2d35c1fbb706a8c1e208908
Dmitry Vasilev <dmitry@t3dev.ru>
2020-02-09 15:29:53 +0300

    Add configuration files

---
+++ public_html/typo3conf/PackageStates.php
@@ -0,0 +1,202 @@
+<?php
+# PackageStates.php
+
+# This file is maintained by TYPO3's package management. Although you can edit it
+# manually, you should rather use the extension manager for maintaining packages.
+# This file will be regenerated automatically if it doesn't exist. Deleting this file
+# should, however, never become necessary if you use the package commands.
+
+return [
+    'packages' => [
+        'core' => [
+            'packagePath' => 'typo3/sysext/core/',
+        ],
+        'scheduler' => [
+            'packagePath' => 'typo3/sysext/scheduler/',
+        ],
+        'extbase' => [
+            'packagePath' => 'typo3/sysext/extbase/',
+        ],
+        'fluid' => [
+            'packagePath' => 'typo3/sysext/fluid/',
+        ],
+        'install' => [
+            'packagePath' => 'typo3/sysext/install/',
+        ],
+        'info' => [
+            'packagePath' => 'typo3/sysext/info/',
+        ],
+        'linkvalidator' => [
+            'packagePath' => 'typo3/sysext/linkvalidator/',
+        ],
+        'frontend' => [
+            'packagePath' => 'typo3/sysext/frontend/',
+        ],
+        'fluid_styled_content' => [
+            'packagePath' => 'typo3/sysext/fluid_styled_content/',
+        ],
+        'indexed_search' => [
+            'packagePath' => 'typo3/sysext/indexed_search/',
+        ],
+        'info_pagetsconfig' => [
+            'packagePath' => 'typo3/sysext/info_pagetsconfig/',
+        ],
+        'extensionmanager' => [
+            'packagePath' => 'typo3/sysext/extensionmanager/',
+        ],
+        'lang' => [
+            'packagePath' => 'typo3/sysext/lang/',
+        ],
+        'recycler' => [
+            'packagePath' => 'typo3/sysext/recycler/',
+        ],
+        'reports' => [
+            'packagePath' => 'typo3/sysext/reports/',
+        ],
+        'setup' => [
+            'packagePath' => 'typo3/sysext/setup/',
+        ],
+        'rte_ckeditor' => [
+            'packagePath' => 'typo3/sysext/rte_ckeditor/',
+        ],
+        'func' => [
+            'packagePath' => 'typo3/sysext/func/',
+        ],
+        'wizard_crpages' => [
+            'packagePath' => 'typo3/sysext/wizard_crpages/',
+        ],
+        'wizard_sortpages' => [
+            'packagePath' => 'typo3/sysext/wizard_sortpages/',
+        ],
+        'version' => [
+            'packagePath' => 'typo3/sysext/version/',
+        ],
+        'workspaces' => [
+            'packagePath' => 'typo3/sysext/workspaces/',
+        ],
+        'about' => [
+            'packagePath' => 'typo3/sysext/about/',
+        ],
+        'backend' => [
+            'packagePath' => 'typo3/sysext/backend/',
+        ],
+        'belog' => [
+            'packagePath' => 'typo3/sysext/belog/',
+        ],
+        'beuser' => [
+            'packagePath' => 'typo3/sysext/beuser/',
+        ],
+        'context_help' => [
+            'packagePath' => 'typo3/sysext/context_help/',
+        ],
+        'cshmanual' => [
+            'packagePath' => 'typo3/sysext/cshmanual/',
+        ],
+        'feedit' => [
+            'packagePath' => 'typo3/sysext/feedit/',
+        ],
+        'filelist' => [
+            'packagePath' => 'typo3/sysext/filelist/',
+        ],
+        'impexp' => [
+            'packagePath' => 'typo3/sysext/impexp/',
+        ],
+        'lowlevel' => [
+            'packagePath' => 'typo3/sysext/lowlevel/',
+        ],
+        'opendocs' => [
+            'packagePath' => 'typo3/sysext/opendocs/',
+        ],
+        'recordlist' => [
+            'packagePath' => 'typo3/sysext/recordlist/',
+        ],
+        'saltedpasswords' => [
+            'packagePath' => 'typo3/sysext/saltedpasswords/',
+        ],
+        'sv' => [
+            'packagePath' => 'typo3/sysext/sv/',
+        ],
+        'sys_note' => [
+            'packagePath' => 'typo3/sysext/sys_note/',
+        ],
+        't3editor' => [
+            'packagePath' => 'typo3/sysext/t3editor/',
+        ],
+        'taskcenter' => [
+            'packagePath' => 'typo3/sysext/taskcenter/',
+        ],
+        'tstemplate' => [
+            'packagePath' => 'typo3/sysext/tstemplate/',
+        ],
+        'viewpage' => [
+            'packagePath' => 'typo3/sysext/viewpage/',
+        ],
+        'flux' => [
+            'packagePath' => 'typo3conf/ext/flux/',
+        ],
+        'fluidcontent' => [
+            'packagePath' => 'typo3conf/ext/fluidcontent/',
+        ],
+        'vhs' => [
+            'packagePath' => 'typo3conf/ext/vhs/',
+        ],
+        'vostokzapadesign_site' => [
+            'packagePath' => 'typo3conf/ext/vostokzapadesign_site/',
+        ],
+        'gridelements' => [
+            'packagePath' => 'typo3conf/ext/gridelements/',
+        ],
+        'bootstrap_grids' => [
+            'packagePath' => 'typo3conf/ext/bootstrap_grids/',
+        ],
+        'static_info_tables' => [
+            'packagePath' => 'typo3conf/ext/static_info_tables/',
+        ],
+        'realurl' => [
+            'packagePath' => 'typo3conf/ext/realurl/',
+        ],
+        'cs_seo' => [
+            'packagePath' => 'typo3conf/ext/cs_seo/',
+        ],
+        'news' => [
+            'packagePath' => 'typo3conf/ext/news/',
+        ],
+        'news_fal_migration' => [
+            'packagePath' => 'typo3conf/ext/news_fal_migration/',
+        ],
+        'news_ttnewsimport' => [
+            'packagePath' => 'typo3conf/ext/news_ttnewsimport/',
+        ],
+        'fancybox' => [
+            'packagePath' => 'typo3conf/ext/fancybox/',
+        ],
+        'lfeditor' => [
+            'packagePath' => 'typo3conf/ext/lfeditor/',
+        ],
+        'my_redirects' => [
+            'packagePath' => 'typo3conf/ext/my_redirects/',
+        ],
+        'powermail' => [
+            'packagePath' => 'typo3conf/ext/powermail/',
+        ],
+        'sourceopt' => [
+            'packagePath' => 'typo3conf/ext/sourceopt/',
+        ],
+        't3devcarousel' => [
+            'packagePath' => 'typo3conf/ext/t3devcarousel/',
+        ],
+        'tmpl' => [
+            'packagePath' => 'typo3conf/ext/tmpl/',
+        ],
+        'tmpl_migration' => [
+            'packagePath' => 'typo3conf/ext/tmpl_migration/',
+        ],
+        'typoscript_code' => [
+            'packagePath' => 'typo3conf/ext/typoscript_code/',
+        ],
+        'ws_scss' => [
+            'packagePath' => 'typo3conf/ext/ws_scss/',
+        ],
+    ],
+    'version' => 5,
+];
