72b29104bab04e3675ff190656cd9428af8b8967
Dmitry Vasilev <dmitry@t3dev.ru>
2020-02-16 14:02:01 +0300

    Feature #369 - EXT:tmpl_migration - method updateBannerCeElement

--- public_html/typo3conf/ext/tmpl_migration/Classes/Command/MigrateContentElementsCommandController.php
+++ public_html/typo3conf/ext/tmpl_migration/Classes/Command/MigrateContentElementsCommandController.php
@@ -17,10 +17,17 @@ class MigrateContentElementsCommandController extends AbstractCommandController
      */
     public function migrateContentElementsCommand() {
 
+        // Update content elements teacher
         $countTeacherCeElement = $this->countUpdatedItems('Fce_Teacher');
         $this->updateTeacherCeElement();
 
+        // Update content elements banner
+        $countBannerCeElement = $this->countUpdatedItems('Fce_Banner');
+        $this->updateBannerCeElement();
+
+        // Messages in dev log
         $msg = " \n Updated {$countTeacherCeElement} content elements 'Fce_Teacher' \n ";
+        $msg .= "Updated {$countBannerCeElement} content elements 'Fce_Banner' \n ";
         $config = $this->getConfiguration();
         if ($config['enableDevLog']) {
             $this->devLog($msg);
@@ -79,7 +86,66 @@ class MigrateContentElementsCommandController extends AbstractCommandController
             ];
 
             $db->exec_UPDATEquery(self::TABLE_migrate, $where, $updateArray, TRUE);
-            //\TYPO3\CMS\Core\Utility\DebugUtility::debug($title);
+
+        }
+    }
+
+
+    /**
+     * Migrate data from content element "Banner Teaser"
+     *
+     * @return void
+     */
+    protected function updateBannerCeElement() {
+
+        $db = $this->getDatabaseConnection();
+        $uids = $this->getElementUids('Fce_Banner');
+
+        foreach($uids as $key => $uid) {
+
+            $row = $db->exec_SELECTgetSingleRow(
+                'pi_flexform',
+                'tt_content',
+                'uid=' . (int)$uid
+            );
+
+            if ($row['pi_flexform'] != '') {
+                $flexform = GeneralUtility::xml2array($row['pi_flexform']);
+            }
+
+            // Banner title
+            if (is_array($flexform) && isset($flexform['data']['banner']['lDEF']['title']['vDEF'])) {
+                $title = $flexform['data']['banner']['lDEF']['title']['vDEF'];
+                $title = '\'' . addslashes($title) . '\'';
+            }
+
+            // Banner text
+            if (is_array($flexform) && isset($flexform['data']['banner']['lDEF']['text']['vDEF'])) {
+                $bodytext = $flexform['data']['banner']['lDEF']['text']['vDEF'];
+                $bodytext = '\'' . addslashes($bodytext) . '\'';
+            }
+
+            // Last banner in the line
+            if (is_array($flexform) && isset($flexform['data']['banner']['lDEF']['isLast']['vDEF'])) {
+                if($flexform['data']['banner']['lDEF']['isLast']['vDEF'] == 1) {
+                    $lastLine = '\'1\'';
+                } else {
+                    $lastLine = '\'0\'';
+                }
+            }
+
+            $where = "`CType` = 'fluidcontent_content' AND `tx_fed_fcefile` = 'Devcompany.VostokzapadesignSite:Fce_Banner.html' AND `uid` = $uid";
+            $updateArray = [
+                '`CType`' => '\'ce_banner\'',
+                '`tx_fed_fcefile`' => ' \'\' ',
+                '`header`' =>  $title,
+                '`bodytext`' => $bodytext,
+                '`is_last`' => $lastLine
+            ];
+
+            $db->exec_UPDATEquery(self::TABLE_migrate, $where, $updateArray, TRUE);
+
+            //\TYPO3\CMS\Core\Utility\DebugUtility::debug($lastLine);
 
         }
 
