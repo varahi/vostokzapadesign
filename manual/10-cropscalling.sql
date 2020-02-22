27f09492ff1ab027fc595067c021ceb0ca7467ab
Dmitry Vasilev <d.vasilev@connecta.ag>
2020-02-22 13:34:30 +0300

    Feature #372 - change manual

--- manual/10-cropscalling.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE tt_content DROP COLUMN cropscaling;
-ALTER TABLE tt_content CHANGE `tx_nwtimagecrop_cropscaling` `cropscaling` tinyint(3);
-
-ALTER TABLE tt_content DROP COLUMN cropscaling_ratio;
-ALTER TABLE tt_content CHANGE `tx_nwtimagecrop_cropscaling_ratio` `cropscaling_ratio` varchar(100);
-
-ALTER TABLE tt_content DROP COLUMN cropscaling_orient;
-ALTER TABLE tt_content CHANGE `tx_nwtimagecrop_cropscaling_orient` `cropscaling_orient` tinyint(3);
